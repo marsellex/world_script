28403aedaa003c110ccb2ae0b28b0f627320c30c
marsellex <135837564+marsellex@users.noreply.github.com>
2025-12-05 12:54:36 +0300

    Delete logic_utils.js

--- logic_utils.js
+++
@@ -1,9 +0,0 @@
-function $(sel) {
-  return document.querySelector(sel);
-}
-
-function createEl(tag, attrs={}) {
-  const el = document.createElement(tag);
-  for (const k in attrs) el[k] = attrs[k];
-  return el;
-}
